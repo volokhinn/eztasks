c52f1505d8b384f72e5fed1e625a2fd93e159c91
volokhinn <csgomx@mail.ru>
2023-06-28 14:02:46 +0700

    App is done

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -3,27 +3,40 @@ import styles from '../scss/app.module.scss';
 import Card from './Card';
 import ButtonAdd from './ButtonAdd';
 import { useSelector, useDispatch } from 'react-redux';
-import { findCardByStatus, addCard } from '../redux/slices/cardSlice';
+import { findCardByStatus, addCard, updateStatusCard, removeCard } from '../redux/slices/cardSlice';
 
+export const Column = ({ status: columnStatus, columnTitle, isAddButton }) => {
+  const cards = useSelector(findCardByStatus(columnStatus));
 
-export const Column = ({status, columnTitle, isAddButton}) => {
+  const dispatch = useDispatch();
+  const onClickAdd = () => {
+    dispatch(addCard());
+  };
 
-    const dispatch = useDispatch();
-const onClickAdd = () => {
-    const newCard = {id: new Date().getTime(), status: 'perform', text: 'Нажмите, чтобы ввести текст'};
-    dispatch(addCard(newCard));
-}
+  const onClickTrash = (id) => {
+    dispatch(removeCard({ id }));
+  };
 
-  const cards = useSelector(findCardByStatus(status));
-  const cardsElements = cards.map((obj) => <Card key={obj.id} {...obj} />);
+  const onClickClock = (id, currentStatus) => {
+    const status = currentStatus === 'perform' ? 'execution' : 'done';
+    dispatch(updateStatusCard({ id, status }));
+    console.log(status);
+  };
+
+  const cardsElements = cards.map((card) => (
+    <Card
+      onClickClock={() => onClickClock(card.id, card.status)}
+      onClickTrash={() => onClickTrash(card.id)}
+      key={card.id}
+      {...card}
+    />
+  ));
   return (
     <div className={styles.cards__column}>
-        <h2 className={styles.cards__title}>{columnTitle}</h2>
-        {cardsElements}
-        {isAddButton && (
-            <ButtonAdd onClick={onClickAdd} />
-        )}
+      <h2 className={styles.cards__title}>{columnTitle}</h2>
+      {cardsElements}
+      {isAddButton && <ButtonAdd onClick={() => onClickAdd()} />}
     </div>
-  )
-}
+  );
+};
 export default Column;
